584e87f733ee428ba7a6c6fd67c1fc3e5b956f41
aristelcrowley <andhikapratamaunsri@gmail.com>
2025-05-08 18:58:07 +0700

    refactor: authentication controller name change

--- back-end/controllers/auth.go
+++ back-end/controllers/auth.go
@@ -1,13 +1,10 @@
 package controllers
 
 import (
-	"os"
-	"time"
 	"taskease/database"
 	"taskease/models"
 
 	"github.com/gofiber/fiber/v2"
-	"github.com/golang-jwt/jwt/v4"
 )
 
 func Register(c *fiber.Ctx) error {
@@ -30,7 +27,7 @@ func Register(c *fiber.Ctx) error {
 	newUser := models.User{
 		Username: username,
 		Password: password,
-		Role:     "user", 
+		Role:     "user",
 	}
 
 	if err := database.DB.Create(&newUser).Error; err != nil {
@@ -48,37 +45,23 @@ func Login(c *fiber.Ctx) error {
 	username := c.FormValue("username")
 	password := c.FormValue("password")
 
-	if username == "" || password == "" {
-		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
-			"error": "Username and password are required",
-		})
-	}
-
 	var user models.User
-	if err := database.DB.Where("username = ? AND password = ?", username, password).First(&user).Error; err != nil {
+	if err := database.DB.Where("username = ?", username).First(&user).Error; err != nil {
 		return c.Status(fiber.StatusUnauthorized).JSON(fiber.Map{
 			"error": "Invalid credentials",
 		})
 	}
 
-	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
-		"sub":  user.UserID,
-		"role": user.Role,
-		"exp":  time.Now().Add(time.Hour * 24).Unix(),
-	})
-
-	tokenString, err := token.SignedString([]byte(os.Getenv("JWT_SECRET_KEY")))
-	if err != nil {
-		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
-			"error": "Failed to generate token",
+	if user.Password != password {
+		return c.Status(fiber.StatusUnauthorized).JSON(fiber.Map{
+			"error": "Invalid credentials",
 		})
 	}
 
 	return c.JSON(fiber.Map{
-		"token": tokenString,
+		"message": "Login successful",
 	})
 }
-
 
 func Logout(c *fiber.Ctx) error {
 	return c.JSON(fiber.Map{
