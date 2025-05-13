0e22e31e891cf3847dd60b5e437d5893b9cf10f3
aristelcrowley <andhikapratamaunsri@gmail.com>
2025-05-13 23:59:28 +0700

    fix: cant cors, add the middleware for it

--- back-end/main.go
+++ back-end/main.go
@@ -8,16 +8,24 @@ import (
     "taskease/routes"
 
 	 "github.com/gofiber/fiber/v2"
+     "github.com/gofiber/fiber/v2/middleware/cors"
 )
 
 func main() {
     config.ENVLoad()
+    database.ConnectDB()
+
+
     app := fiber.New()
 
-    database.ConnectDB()
+    feport := os.Getenv("FE_SERVER_PORT")
+    app.Use(cors.New(cors.Config{
+		AllowOrigins: "http://localhost:"+ feport, 
+		AllowHeaders: "Origin, Content-Type, Accept",
+	}))
 
     routes.SetupRoutes(app)
 
-    port := os.Getenv("SERVER_PORT")
-	app.Listen(":"+port)
+    beport := os.Getenv("BE_SERVER_PORT")
+	app.Listen(":"+beport)
 }
