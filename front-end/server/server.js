03f03c6af3efc5822f0169d6c43e51ef21d9d9b8
aristelcrowley <andhikapratamaunsri@gmail.com>
2025-05-14 12:25:22 +0700

    refactor: change from using .html pages to the routes itself

--- front-end/server/server.js
+++ front-end/server/server.js
@@ -4,10 +4,23 @@ const path = require("path");
 const app = express();
 const PORT = 3000;
 
+// Serve static files (HTML, CSS, JS, images, etc.)
 app.use(express.static(path.join(__dirname, "../")));
 
-app.get("*", (req, res) => {
+// Serve home.html only for root route
+app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "../", "home.html"));
+});
+
+app.get("/login", (req, res) => {
+  res.sendFile(path.join(__dirname, "../", "login.html"));
+});
+app.get("/register", (req, res) => {
+  res.sendFile(path.join(__dirname, "../", "register.html"));
+});
+
+app.use((req, res) => {
+  res.status(404).send("Page not found");
 });
 
 app.listen(PORT, () => {
